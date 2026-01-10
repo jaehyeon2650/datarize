2086194590e2a5d074bcf6679e7046ee7d3f1c97
wogus <ind07152@naver.com>
2026-01-10 14:25:22 +0900

    chore: initial commit

---
+++ src/main/java/org/example/datarize/DatarizeApplication.java
@@ -0,0 +1,13 @@
+package org.example.datarize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatarizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatarizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/datarize/DatarizeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datarize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
